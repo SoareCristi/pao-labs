98c0340297988bab3c639849c8875e4dd729e7d9
SoareCristi <cristi2001s@gmail.com>
2022-02-24 19:25:05 +0200

    lab_0_1

--- lab_0/src/com/company/Main.java
+++ lab_0/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
+//imports
+import com.company.models.Student;
+import com.company.models.Teacher;
+//import com.company.models.* importa toate clasele din pachet
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello");
+	Student student = new Student();
+    Teacher teacher = new Teacher();
+    System.out.println(":(");
     }
 }
